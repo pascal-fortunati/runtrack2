22e69c15ca66a809d868fd84afc265330bcfcaf7
Zeigadis <pascal.fortunati@laplateforme.io>
2025-09-16 14:35:35 +0200

    Update index.php

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -4,14 +4,14 @@ $hauteur = 5; // Hauteur du triangle
 for ($i = 1; $i <= $hauteur; $i++) {
     // Afficher les espaces pour centrer le triangle
     for ($j = 1; $j <= $hauteur - $i; $j++) {
-        echo "&nbsp;&nbsp;"; // deux espaces HTML pour un meilleur alignement
+        echo "&nbsp;&nbsp;";
     }
 
     // Afficher les étoiles pour cette ligne
     for ($k = 1; $k <= $i; $k++) {
         echo "*";
-        if ($k < $i) echo " "; // ajouter un espace entre les étoiles sauf après la dernière
+        if ($k < $i) echo " ";
     }
 
-    echo "<br />"; // saut de ligne après chaque ligne
+    echo "<br />";
 }
