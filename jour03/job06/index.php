c062e23254e2194897798c44d06cdea6a6d3c244
Zeigadis <pascal.fortunati@laplateforme.io>
2025-09-17 15:40:09 +0200

    Update index.php

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,8 +1,14 @@
 <?php
+// Déclaration de l'encodage UTF-8 pour le navigateur
+header('Content-Type: text/html; charset=utf-8');
+
 // Déclaration de la chaîne
 $str = "Les choses que l'on possède finissent par nous posséder.";
 
-// Parcours de la chaîne à l'envers et affichage
-for ($i = strlen($str) - 1; $i >= 0; $i--) {
-    echo $str[$i];
+// Transformer la chaîne en tableau de caractères UTF-8
+$chars = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
+
+// Parcours à l'envers et affichage
+for ($i = count($chars) - 1; $i >= 0; $i--) {
+    echo $chars[$i];
 }
