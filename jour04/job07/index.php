0876ae7099533dee726781bbaa0a08afa70969d6
Zeigadis <pascal.fortunati@laplateforme.io>
2025-09-19 14:29:58 +0200

    refactorisation job7

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,34 +1,27 @@
 <?php
-if (isset($_POST['largeur']) && isset($_POST['hauteur'])) {
+if (isset($_POST['largeur'], $_POST['hauteur'])) {
     $largeur = (int)$_POST['largeur'];
     $hauteur = (int)$_POST['hauteur'];
 
     if ($largeur > 1 && $hauteur > 0) {
         $output = '';
-        // Calcul du toit (triangle)
+        // Calcul du toit
         $toitHauteur = ceil($largeur / 2);
         for ($i = 0; $i < $toitHauteur; $i++) {
-            $nbEtoiles = 1 + 2 * $i;
-            if ($nbEtoiles > $largeur) $nbEtoiles = $largeur;
+            $nbEtoiles = min(1 + 2 * $i, $largeur);
             $nbEspaces = floor(($largeur - $nbEtoiles) / 2);
-            $output .= str_repeat(' ', $nbEspaces);
-            $output .= str_repeat('*', $nbEtoiles);
-            $output .= "\n";
+            $output .= str_repeat(' ', $nbEspaces) . str_repeat('*', $nbEtoiles) . "\n";
         }
 
         // Corps de la maison
         for ($i = 0; $i < $hauteur; $i++) {
-            if ($largeur == 1) {
-                $output .= "*\n";
-            } else {
-                $output .= '*' . str_repeat(' ', $largeur - 2) . '*' . "\n";
-            }
+            $output .= '*' . str_repeat(' ', $largeur - 2) . '*' . "\n";
         }
 
         // Sol de la maison
         $output .= str_repeat('*', $largeur) . "\n";
 
-        // Affichage dans une balise préformatée
+        // Affichage
         echo "<pre>$output</pre>";
     } else {
         echo "Largeur et hauteur doivent être des nombres positifs (largeur > 1).";
